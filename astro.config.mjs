2ad515703d50e9cc0423aeda55f3d6f03827f8b6
Yew02 <zongyung6656@gmail.com>
2025-07-27 14:31:04 +0800

    add tailwind css plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 import bun from "@nurodev/astro-bun";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [solidJs()],
   adapter: bun(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
