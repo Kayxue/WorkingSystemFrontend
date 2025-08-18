cf6ec55878ea98fc5843ce3c68c32aba0bc21398
Kayxue <s0990006@gmail.com>
2025-08-18 23:24:10 +0800

    Change adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
-import bun from "@nurodev/astro-bun";
+import bun from "@hedystia/astro-bun";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
