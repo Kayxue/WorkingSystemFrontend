fa34fb3a6925bd5a7170516f1bdde2674197e3b8
Kayxue <s0990006@gmail.com>
2025-08-18 23:35:48 +0800

    Change server ip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
-import bun from "@hedystia/astro-bun";
+import bun from "@nurodev/astro-bun";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [solidJs()],
-  adapter: bun(),
+  adapter: bun({host:"0.0.0.0", port: 4321}),
   vite: {
     plugins: [tailwindcss()],
   },
